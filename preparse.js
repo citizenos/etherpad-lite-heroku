3276d2c69c54fcfc50954c7de74ca9e0ebdf7e20
ilmar <ilmar.tyrk@gmail.com>
2018-04-16 12:08:18 +0300

    preparse with log

--- preparse.js
+++ preparse.js
@@ -46,11 +46,11 @@ childProcess.execSync('./installPackages.sh');
 
 // Run the Etherpad itself. Using kexec so that the current process would get replaced with the new one
 if (process.env.ETHERPAD_ALLOW_ROOT) {
-    kexec('./etherpad-lite/bin/safeRun.sh log.log --root');
+    kexec('./etherpad-lite/bin/run.sh log.log --root');
     kexec('kill node');
     kexec('node --optimize_for_size --max_old_space_size=460 --gc_interval=100 ./etherpad-lite/node_modules/ep_etherpad-lite/node/server.js ');
 } else {
-    kexec('./etherpad-lite/bin/safeRun.sh log.log');
+    kexec('./etherpad-lite/bin/run.sh log.log');
     kexec('kill node');
     kexec('node --optimize_for_size --max_old_space_size=460 --gc_interval=100 ./etherpad-lite/node_modules/ep_etherpad-lite/node/server.js ');
 } 
