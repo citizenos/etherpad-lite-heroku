b0c1beab81dd83bd4139c83aa9c8540d208f3b58
Mikk Andresen <mikk.andresen@gmail.com>
2018-05-29 18:17:11 +0300

    Force Heroku rebuild.

--- start.js
+++ start.js
@@ -113,4 +113,5 @@ if (process.env.ETHERPAD_ALLOW_ROOT) {
     kexec('./etherpad-lite/bin/run.sh');
     kexec('kill node');
     kexec('node --optimize_for_size --max_old_space_size=460 --gc_interval=100 ./etherpad-lite/node_modules/ep_etherpad-lite/node/server.js ');
-} 
+}
+
